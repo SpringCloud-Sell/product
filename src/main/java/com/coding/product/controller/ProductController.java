06d62e3cea0f843b8de88d07e9d66078f9f73953
Rushing <liming20110711@163.com>
2018-07-30 07:48:31 +0800

    update:20180730-01

--- src/main/java/com/coding/product/controller/ProductController.java
+++ src/main/java/com/coding/product/controller/ProductController.java
@@ -39,12 +39,6 @@ public class ProductController {
 
     @GetMapping("/list")
     public ResultVO<ProductVO> list() {
-        /*
-         * 1. 查询所有上架的商品
-         * 2. 获取类目type列表
-         * 3. 查询类目
-         * 4. 构造数据
-         */
         // 1. 查询所有上架的商品
         List<ProductInfo> productInfoList = productService.findUpAll();
         // 2. 获取类目type列表
