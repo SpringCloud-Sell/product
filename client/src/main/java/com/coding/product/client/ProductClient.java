be381d219106cfff9293f5312b8659365eef02a6
Rushing <liming20110711@163.com>
2018-08-06 08:23:03 +0800

    update:20180806-01

--- client/src/main/java/com/coding/product/client/ProductClient.java
+++ client/src/main/java/com/coding/product/client/ProductClient.java
@@ -15,13 +15,14 @@ package com.coding.product.client;
 import com.coding.product.common.DecreaseStockInput;
 import com.coding.product.common.ProductInfoOutput;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "product")
+@FeignClient(name = "product", fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
 
     @GetMapping("/msg")
@@ -32,4 +33,20 @@ public interface ProductClient {
 
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<DecreaseStockInput> cartDTOList);
+
+    @Component
+    class ProductClientFallback implements ProductClient {
+        @Override
+        public String productMsg() {
+            return null;
+        }
+
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {}
+    }
 }
